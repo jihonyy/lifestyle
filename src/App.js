2dfd8d6ed0cdb0f4204cc0e902504bc4c1b5b9be
jihonyy <muju1400@gmail.com>
2022-04-05 15:00:35 +0900

    update of prettier

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import styled from 'styled-components';
-import { HashRouter, Route } from "react-router-dom";
+import styled from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
-
 
 function App() {
   return (
-    <Container> 
-      <Title>Life Style <br/>라이프 스타일</Title>
+    <Container>
+      <Title>Life Style</Title>
+      <div></div>
       <Content>
-        {/* <Link> */}
-          <Button style={{marginBottom:'15px'}}>라이프 스타일 시작</Button>
-        {/* </Link> */}
-          <Button style={{background: '#FF6B73', color: 'white'}}>기존 회원으로 로그인</Button>
-          <SNS >
-            <img src="../main/facebook.svg"/>
-            <img src="../main/kakao.svg"/>
-            <img src="../main/apple.svg"/>
-            <img src="../main/naver.svg"/>
-            <img src="../main/google.svg"/>
-          </SNS>
-          <Text>SNS 간편로그인</Text>
+        <Link>
+          <Button style={{ marginBottom: "15px" }}>라이프 스타일 시작</Button>
+        </Link>
+        <Button style={{ background: "#FF6B73", color: "white" }}>
+          기존 회원으로 로그인
+        </Button>
+        <SNS>
+          <img src="../main/facebook.svg" />
+          <img src="../main/kakao.svg" />
+          <img src="../main/apple.svg" />
+          <img src="../main/naver.svg" />
+          <img src="../main/google.svg" />
+        </SNS>
+        <Text>SNS 간편로그인</Text>
       </Content>
     </Container>
   );
 }
 
 export default App;
-
 
 const Container = styled.div`
   display: flex;
@@ -40,7 +41,7 @@ const Container = styled.div`
   right: 50%;
   gap: 50px;
   color: white;
-`
+`;
 
 const Title = styled.div`
   color: white;
@@ -49,13 +50,13 @@ const Title = styled.div`
   height: 115px;
   font-weight: bold;
   font-size: 48px;
-`
+`;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const Button = styled.div`
   display: flex;
@@ -63,15 +64,15 @@ const Button = styled.div`
   justify-content: center;
   width: 177px;
   height: 35px;
-  background: #FFFFFF;
+  background: #ffffff;
   border-radius: 16px;
 
-  color: #FF6B73;
+  color: #ff6b73;
   font-weight: bold;
   font-size: 13px;
 
   cursor: pointer;
-`
+`;
 
 const SNS = styled.div`
   display: flex;
@@ -80,9 +81,8 @@ const SNS = styled.div`
   margin-top: 30px;
   margin-bottom: 25px;
   cursor: pointer;
-
-`
+`;
 
 const Text = styled.div`
   font-size: 15px;
-`
+`;
